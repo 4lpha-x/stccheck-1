b8327999a17daedd7c703e085a3a924551b2b9b5
Rahul rc <60568652+HACKE-RC@users.noreply.github.com>
2020-11-29 10:09:37 +0530

    Update stc.py

--- stc.py
+++ stc.py
@@ -1,46 +1,54 @@
 import urllib3
 import os
+import sys
+try:
+    import pyfiglet
+except:
+    os.system("pip3 install pyfiglet")
+def help():
+    print(pyfiglet.figlet_format('STCCheck'))
+	sys.exit()
+try:
+	list = sys.argv[1]
+except:
+	help()
+list=sys.argv[1]
 try:
 	import requests
 except:
 	import os
-	os.system('pip install requests')
+	os.system('pip3 install requests')
 	os.system('clear')
 import threading
 import socket
-print('\033[1;33;40m------------[+] Status Code Checker By RC [+]----------\n')
-fname = input('\033[1;37;40mEnter The File Name Which Contains The Subdomains \n\n\033[1;32;40mroot@rc #~ ')
+fname = list
 http = urllib3.PoolManager()
 try:
 	f=open(fname)
-	g=open(fname)
 except:
 	import os
 	os.system('clear')
 	print("\033[1;34;40m[+] File Not Found [+]")
-	quit
-	#f=open(fname)
-
+	exit()
+f = open(fname)
 def full(i):
 	i = i.replace('\n','')
 	try:
 		resp=requests.get(i)
 		if resp.status_code==200:
-			print(f'\033[1;33;40m{i}  200 OK')
+			print(f'{i} 200 OK')
 		elif resp.status_code==301:
-			print(f'\033[1;32;40m;;{i} Redirected 301')
+			print(f'{i} Redirecting 301')
 		elif resp.status_code==404:
-			print(f'\033[1;31;40m{i} 404 Not Found')
+			print(f'{i} 404 Not Found')
 		elif resp.status_code==403:
-			print(f"\033[1;36;40m{i} 403 Forbidden/Redirecting")
+			print(f"{i} 403 Forbidden")
 		elif resp.status_code==400:
-			print(f'\033[1;30;40m{i} 400 Bad Request')
+			print(f'{i} 400 Bad Request')
 		else:
-			print(f"\033[1;34;40m{i} Status : {resp.status_code}")
+			print(f"{i} Status : {resp.status_code}")
 	except requests.ConnectionError:
-		print("\033[1;31;40m" + i + " 404 Not Found")
-	
-	
+		pass
 for i in f:
 	thread = threading.Thread(target=full,args=(i,))
 	thread.start()
